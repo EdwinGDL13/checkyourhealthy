546c748b974cfc2cddacad7400c652769ec5b670
EdwinGDL13 <edwinsalasmorales13@gmail.com>
2025-07-17 11:00:38 -0600

    Cuarto  commit - Proyecto CheckYourHealthy

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,18 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:control_medicacion_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Check initial screen shows RegisterPage', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(MedicApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verificamos que el texto o widget de la pantalla de registro está presente
+    expect(
+      find.text('exitoso'),
+      findsOneWidget,
+    ); // Cambia 'Registrar' por algún texto que esté en tu pantalla RegisterPage
   });
 }
